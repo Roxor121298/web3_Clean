73b2d43fc61eb51470e6c61285200246426de9bf
Edouard Deschamps <edouarddeschamps@gmail.com>
2025-03-05 10:39:20 -0500

    ajout ajax non bugger

--- Exercice 1 - SmartHome v2/js/javascript.js
+++ Exercice 1 - SmartHome v2/js/javascript.js
@@ -1,5 +1,15 @@
 window.addEventListener("load", () => {
-    document.querySelector(".btn-refresh").onclick = () => {
-        window.location.href = "index.php";
-    }
+    updateLights();
 })
+
+const updateLights = () => {
+    fetch("ajax.php", {})
+    .then(response => response.json())
+    .then(response => {
+        response.forEach(light => {
+            document.querySelector("#light" + light.id).src = "img/light-" + (light.status == "1" ? "on" : "off") + ".png";  
+        })
+        setTimeout(updateLights, 5000);
+    });
+    
+}
